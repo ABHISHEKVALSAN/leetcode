bc94ebbb5f99791593a379a64d4504118456b416
ABHISHEKVALSAN <abhishekvalsan9999@gmail.com>
2023-06-16 04:19:43 +0530

    remove bad solution

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        for i,e in enumerate(nums2):
-            nums1[m+i] = e
-        nums1.sort()
