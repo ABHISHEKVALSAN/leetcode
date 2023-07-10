65d9c13188f0b1f54d121cfbf597c0b5e323c7b2
ABHISHEK VALSAN <abhishekvalsan9999@gmail.com>
2023-07-11 00:02:40 +0530

    Time: 58 ms (88.72%), Space: 16.6 MB (65.87%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,21 +1,9 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         
-        i = 0
-        j = 0
-        magazine = sorted(list(magazine))
-        ransomNote = sorted(list(ransomNote))
-        n1 = len(magazine)
-        n2 = len(ransomNote)
-        print(magazine)
-        while i < n1:
-            if j==n2:
-                return True
-            if magazine[i]==ransomNote[j]:
-                i+=1
-                j+=1
+        for ch in ransomNote:
+            if ch in magazine:
+                magazine = magazine.replace(ch,'',1)
             else:
-                i+=1
-        if j==n2:
-            return True
-        return False
+                return False
+        return True
