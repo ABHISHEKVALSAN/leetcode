d6c595197372bda39550782d89ffb8ca5629917f
ABHISHEK VALSAN <abhishekvalsan9999@gmail.com>
2023-07-12 21:13:36 +0530

    Time: 99 ms (83.72%), Space: 17.1 MB (11.08%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,35 +1,39 @@
 class Solution:
 
-    def is_substring(self,s: str,l: int,i: int,wd: dict):
-        n = len(s)
-        d = wd.copy()
-        while i < n:
-            curr_word = s[i:i+l]
-            if curr_word in d.keys():
-                if d[curr_word]==1:
-                    d.pop(curr_word)
-                else:
-                    d[curr_word]-=1
-            else:
-                break
-            i+=l
-        if len(d)==0:
-            return True
-        return False
-
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         n = len(s)
         m = len(words)
-        l = len(words[0])
-        wd = {}
+        word_len = len(words[0])
+        word_freq = {}
         for word in words:
-            if word in wd.keys():
-                wd[word]+=1
-            else:
-                wd[word] = 1
-
-        result = []
-        for i in range(n-l+1):
-            if self.is_substring(s,l,i,wd):
-                result.append(i)
-        return result
+            word_freq[word] = word_freq.get(word,0)+1
+        left_index = 0
+        right_index = 0
+        ans = []
+        ss_word_freq = {}
+        i = 0
+        while i < word_len:
+            left_index = i
+            while left_index <= len(s) - len(words)*len(words[0]):
+                right_index = left_index
+                ss_word_freq = {}
+                while right_index < len(s):
+                    word = s[right_index:right_index+word_len]
+                    if word in word_freq:
+                        ss_word_freq[word] = ss_word_freq.get(word,0)+1
+                        if ss_word_freq==word_freq:
+                            ans.append(left_index)
+                            left_word = s[left_index:left_index+word_len]
+                            ss_word_freq[left_word]-=1
+                            left_index+=word_len
+                        else:
+                            while ss_word_freq[word] > word_freq[word]:
+                                left_word = s[left_index:left_index+word_len]
+                                ss_word_freq[left_word]-=1
+                                left_index+=word_len
+                    else:
+                        break
+                    right_index+=word_len
+                left_index+=word_len
+            i+=1
+        return ans
