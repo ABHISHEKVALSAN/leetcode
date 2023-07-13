0687ca9fde82eeba2453f8e6a2a10616aa7fa026
ABHISHEK VALSAN <abhishekvalsan9999@gmail.com>
2023-07-14 02:30:45 +0530

    Time: 1380 ms (82%), Space: 0B (100%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+with temp as (
+    select 
+        min(id) as id,
+        email
+    from 
+        Person
+    group by 
+        email
+)
+delete 
+from 
+    Person
+where id not in (select id from temp)
+
