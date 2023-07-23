34e4d51b7931c4623e04e7828d4df2363b4a9d9c
ABHISHEK VALSAN <abhishekvalsan9999@gmail.com>
2023-07-24 05:00:56 +0530

    Time: 51 ms (96.85%), Space: 18.7 MB (31.84%) - LeetHub

--- 0836-race-car/0836-race-car.py
+++ 0836-race-car/0836-race-car.py
@@ -1,23 +1,23 @@
 from collections import deque
 class Solution:
     def racecar(self, target: int) -> int:
-        queue = deque()
-        queue.append((0,0,1))
-        visited = set()
-        while queue:
-            step, pos, speed = queue.popleft()
-            if pos==target:
-                return step
-            if (pos,speed) not in visited:
-                visited.add((pos,speed))
-                queue.append((step+1,pos+speed,speed*2))
-                if (pos+speed > target and speed>0) or \
-                    (pos+speed<target and speed<0):
-                    speed = -1 if speed > 0 else 1
-                    queue.append((step+1,pos,speed))
-        return 0
+        def solve(pos,speed,target):
+            queue = deque()
+            queue.append([0,1,0])
+            visited = set()
+            while queue:
+                pos, speed, result = queue.popleft()
+                if pos==target:
+                    return result
+                if (pos,speed) not in visited:
+                    visited.add((pos,speed))
+                    queue.append([pos+speed,speed*2,result+1])
+                if pos+speed > target and speed > 0:
+                    queue.append([pos,-1,result+1])
+                if pos+speed < target and speed < 0:
+                    queue.append([pos,1,result+1])
+            return 0
 
-            
-        return dp[target]
+        return solve(0,1,target)
 
 
