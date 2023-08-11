32ea814bbadfd2774b1dcf991e568f6742e8bc42
ABHISHEK VALSAN <abhishekvalsan9999@gmail.com>
2023-08-11 08:53:54 +0530

    Time: 141 ms (97.52%), Space: 16.7 MB (62%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -4,13 +4,10 @@ class Solution:
             return True
         l = len(flowerbed)
         flowerbed = [0] + flowerbed + [0]
-        fc = 0
-        i = 1
-        while i <= l:
+        for i in range(1,l+1):
             if flowerbed[i-1]==flowerbed[i+1]==flowerbed[i]==0:
                 flowerbed[i]=1
-                fc+=1
-                if fc>=n:
+                n-=1
+                if n==0:
                     return True
-            i+=1
         return False
