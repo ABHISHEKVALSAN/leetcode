b423799eb2c9494d0ce34fad38877f2326a012d2
ABHISHEK VALSAN <abhishekvalsan9999@gmail.com>
2023-08-15 08:53:27 +0530

    Time: 37 ms (93.69%), Space: 16.3 MB (84.23%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -7,13 +7,16 @@ class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
         big = head
         small = head
-        while big is not None and big.val < x:
-            big = big.next
-        while small is not None and small.val >= x:
+        while small and small.val >= x:
             small = small.next
+        while big and big.val <x:
+            big = big.next
         if small is None or big is None:
-            return head
-        
+            if small:
+                return small
+            else:
+                return big
+
         small_ptr = small
         big_ptr = big
         if small==head:
@@ -24,8 +27,9 @@ class Solution:
             while big_ptr.next!=small:
                 big_ptr = big_ptr.next
             big_ptr.next = small.next
-        while small_ptr is not None and big_ptr is not None:
-            if small_ptr.next is not None:
+        
+        while small_ptr and big_ptr:
+            if small_ptr.next:
                 if small_ptr.next.val < x:
                     small_ptr = small_ptr.next
                     big_ptr.next = small_ptr.next
